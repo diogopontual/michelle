89af9fe3772fdad57f7d3bc8af9ab732b24ef5f9
Diogo Dauster Pontual <diogopontual@gmail.com>
2020-02-26 14:42:06 -0300

    removing

--- src/cnpj.js
+++ src/cnpj.js
@@ -1,29 +1,31 @@
-const fs = require('fs');
-const unzipper = require('unzipper');
-const homedir = require('os').homedir();
+const fs = require("fs");
+const unzipper = require("unzipper");
+const homedir = require("os").homedir();
 
-const folder=process.argv[2];
+const folder = process.argv[2];
 
 begin();
 
-async function begin(){
-  fs.readdir(folder, async function (err, files) {
+async function begin() {
+  fs.readdir(folder, async function(err, files) {
     if (err) {
-        return console.log('Unable to scan directory: ' + err);
-    } 
-    for (const file of files) {
-      await unzipFile(folder + file, file)  
+      return console.log("Unable to scan directory: " + err);
     }
-});
+    for (const file of files) {
+      if (file.indexOf(".zip") > 0) await unzipFile(folder + file, file);
+    }
+  });
 }
-async function unzipFile(file, filename){
-  await fs.createReadStream(file).pipe(unzipper.Extract({ path: homedir + '/ziptmp/' + filename }));  
+async function unzipFile(file, filename) {
+  await fs
+    .createReadStream(file)
+    .pipe(unzipper.Extract({ path: homedir + "/ziptmp/" + filename }));
 }
 
 function delay(t, val) {
   return new Promise(function(resolve) {
-      setTimeout(function() {
-          resolve(val);
-      }, t);
+    setTimeout(function() {
+      resolve(val);
+    }, t);
   });
 }
