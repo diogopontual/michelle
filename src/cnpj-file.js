c0bc974a1f94948dc3cf024e974a311c9c22a801
Diogo Dauster Pontual <diogopontual@gmail.com>
2020-02-26 15:43:50 -0300

    done

---
+++ src/cnpj-file.js
@@ -0,0 +1,43 @@
+const lineReader = require("line-reader");
+const { Client } = require("@elastic/elasticsearch");
+const client = new Client({ node: "http://138.68.232.187:9200" });
+let bulk = [];
+count = 0;
+lineReader.eachLine(process.argv[2], function(line, last) {
+  if (line.charAt(0) == "1") {
+    count++;
+    //empresa
+    let obj = {
+      tipo: line.charAt(17), //1 = matriz, 2 = filial
+      nomeEmpresarial: line.substr(18, 150).trim(),
+      nomeFantasia: line.substr(168, 55).trim(),
+      situacao: line.substr(223, 2),
+      cnae: line.substr(375, 7),
+      porte: line.substr(905, 2),
+      capital: Number.parseInt(line.substr(891, 14)),
+      cnpj: line.substr(3, 14)
+    };
+
+    bulk.push({ index: { _index: "cnpj" } }, obj);
+  }
+  if (bulk.length > 400 || last) {
+    client.bulk(
+      {
+        index: "cnpj",
+        body: bulk
+      },
+      function(err, resp) {
+        if (err) {
+          console.log(err);
+          callback(err);
+        } else {
+        //   console.log("items", resp.body.items.length);
+        }
+      }
+    );
+    bulk = [];
+  }
+  if (last) {
+    console.log("total", count);
+  }
+});
