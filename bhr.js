f57b0df4a4f285e188bd7cc6f49613a71cac58ce
Diogo Dauster Pontual <diogopontual@gmail.com>
2020-03-06 16:06:56 -0300

    downloading

--- bhr.js
+++ bhr.js
@@ -1,12 +1,12 @@
-const wget = require("node-wget");
+var urllib = require("urllib");
 const cheerio = require("cheerio");
 const alphabet = "0abcdefghijklmnopqrstuvwxyz";
 const baseUrl = "https://www.business-humanrights.org/en";
 for (let i = 0; i < alphabet.length; i++) {
-  wget(
+  urllib.request(
     `${baseUrl}/find-companies?letter=${alphabet.charAt(i)}`,
-    (err, response, body) => {
-      $ = cheerio.load(body);
+    (err, data, res) => {
+      $ = cheerio.load(data);
       $("tr.company_list_row td a.company_name").each((idx, el) => {
         let name = $(el).text();
         let url = $(el).attr("href");
@@ -17,18 +17,14 @@ for (let i = 0; i < alphabet.length; i++) {
 }
 
 function loadCompany(name, url) {
-  if(url.indexOf('/ru/') >= 0)
-    return;
+  if (url.indexOf("/ru/") >= 0) return;
   let page = 0;
   let fullURL = `${baseUrl}${url}?/?dateorder=datedesc&page=${page}&componenttype=all`;
   try {
-    wget(fullURL, (err, response, body) => {
-        console.log(err);
-        console.log(`${name}: ${url}`);
-    });
-  }catch(err){
-      console.log(`error`);
-      console.log(fullURL);
-      console.log(err);
+    urllib.request(fullURL, (err, data, res) => {});
+  } catch (err) {
+    console.log(`error`);
+    console.log(fullURL);
+    console.log(err);
   }
 }
